f88d8b2d6e3a3f70efe3224e028e863a7b6433b0
MEvan774 <milanevan@gmail.com>
2021-01-18 15:26:47 +0100

    Updated sniper

--- FPSAsset/Assets/DSR50.cs
+++ FPSAsset/Assets/DSR50.cs
@@ -1,11 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Animations;
 
 public class DSR50 : WeaponParent
 {
     protected override void IsAiming()
     {
         //Make sniper invisible
+    }
+
+    protected override void Fire()
+    {
+        base.Fire();
+        if (currentAmmo != 0)
+        {
+            animator.SetTrigger("AfterShotReloading");
+        }
     }
 }
